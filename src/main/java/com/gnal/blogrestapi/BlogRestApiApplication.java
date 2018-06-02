598bdb439f57f12654fbb83fa45f79774e58b75c
Axel HEINE <axheine@gmail.com>
2018-06-02 18:58:05 +0200

    Init project with Maven

---
+++ src/main/java/com/gnal/blogrestapi/BlogRestApiApplication.java
@@ -0,0 +1,12 @@
+package com.gnal.blogrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogRestApiApplication.class, args);
+	}
+}
